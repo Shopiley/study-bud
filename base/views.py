00a88e9ebc634af5ab783c3e8c32733053e349bc
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2023-06-01 20:15:20 +0100

    set up apps

--- base/views.py
+++ base/views.py
@@ -1,9 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# can be function based or class based 
+
+rooms = [
+    {'id': 1, 'name':'Lets learn python!'},
+    {'id': 2, 'name':'Lets learn javascript!'},
+    {'id': 3, 'name':'backend developers'}
+]
 
 def home(request):
-    return HttpResponse('Home page')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse('Room')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
